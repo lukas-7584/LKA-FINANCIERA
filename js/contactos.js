6b95ad4857a23deb5fedc65a3e41e0d4a1a5bc2f
lucas <amaya.jorgelucas@gmail.com>
2021-10-20 16:38:50 -0300

    se modifico el js en base al desafrio clase 8

--- js/contactos.js
+++ js/contactos.js
@@ -1,13 +1,15 @@
+function validarConsulta () {
 
-let  nombre = prompt("ingrese su nombre");
-let email  = prompt("ingrese su email");
-let  telefono =  parseInt( prompt("ingrese su número de telefono") );
-let comoConocio = prompt("ingrese como nos conocio facebook , twitter , instagram");
-let consulta  = prompt("ingrese su consulta");
+    let  nombre = document.getElementById ("nombre").value;
+    let email  = document.getElementById("inputEmail4").value;
+    let  telefono =   document.getElementById("telefono").value;
+    let comoConocio =  document.getElementById("redesSociales").value;
+    let consulta  =  document.getElementById("consulta").value;
 
-if (nombre || email || telefono != "") {
-    console.log ("Debe ingresar los datos de contacto")
-}
-else{
-console.log("En breve nos estaremos comunicando con usted, muchas gracias por elegirnos")
+    if (nombre || email || telefono || comoConocio || consulta != "") {
+        alert("En breve nos estaremos comunicando con usted, muchas gracias por elegirnos")
+    }
+
+    
+
 }
